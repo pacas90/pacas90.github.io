bd1fbec8c809a1422cf9aa3fcfa8435f24b9ec09
surelis00 <68562977+surelis00@users.noreply.github.com>
2020-11-23 11:29:24 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -192,8 +192,8 @@ document.getElementById("random-color").oninput = function() {
   
 
 canvas2.onclick = function(event) {
-var x = Math.floor(event.offsetX / 3.2) * 3.2;
-var y = Math.floor(event.offsetY / 3.2) * 3.2;
+var x = Math.floor(event.offsetX / 16) * 16;
+var y = Math.floor(event.offsetY / 16) * 16;
 if(currentDrawingMode === "Draw mode") {
 if(document.getElementById("random-color").checked) {
     ctx2.fillStyle = "#" + randomColor();
@@ -208,9 +208,9 @@ if(document.getElementById("random-color").checked) {
    ctx2.fillStyle = ctx2ColorInput.value;
  }
  
-ctx2.fillRect(x,y,4,4);
+ctx2.fillRect(x,y,16,16);
 } else if(currentDrawingMode === "Erase mode") {
-  ctx2.clearRect(x,y,4,4);
+  ctx2.clearRect(x,y,16,16);
 }
  else if(currentDrawingMode === "Fill mode") {
    if(document.getElementById("random-color").checked) {
@@ -225,7 +225,7 @@ ctx2.fillRect(x,y,4,4);
  else if(ctx2ColorInput != "") {
    ctx2.fillStyle = ctx2ColorInput.value;
  }
-   ctx2.fillRect(0,0,32,32);
+   ctx2.fillRect(0,0,16,16);
  }
 
 
